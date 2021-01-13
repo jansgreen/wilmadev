1ccbc78eb573e7098a618b391be9e58edda533af
Luis Pena <Luis Pena>
2021-01-13 10:25:47 -0500

    terminado el boton de comprar servicio ahora

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,38 @@
-$(document).ready(function(){
-  setInterval(function(){
-    var div = $("#ex5");
-    div.animate({height: '300px', width: '300px'}, "slow");
-    div.animate({height: '100px', width: '100px'}, "slow");
-    div.animate({height: '300px', width: '300px'}, "slow");
-    div.animate({height: '100px', width: '100px'}, "slow");
-  }, 5000);
+/*$(document).ready(function () {
+  $("#ex5").toggle("scale", 500);
+});*/
+$(document).ready(function() {	
+  setInterval(function(){ 
+    $("#ex5").ready(function(){
+    $("#ex5").hide(1000);
+  });
+ 
+  $("#ex5").ready(function(){
+    $("#ex5").show(1000);
+  });
+}, 7000);
 });
+
+
+
+
+/*
+
+  setInterval(function pulse() {
+  $("#ex5").animate(
+    { width: 300, height: 30, opacity: 0.5 },
+    700,
+    function () {
+      $("#ex5").animate(
+        { width: 350, height: 45, opacity: 1 },
+        700,
+        function () {
+          pulse();
+        }
+      );
+    },
+  );
+}, 7000);
+
+
+*/
